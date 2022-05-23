9e09ff7ed04c427cf85e56b2d1f94ae606fff6ae
ejgandelaberon <ejgandelaberon@yahoo.com>
2022-05-24 00:56:37 +0800

    add initial styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'task-tracker-app';
+  title: string = 'Task Tracker App';
 }
